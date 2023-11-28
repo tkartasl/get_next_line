188e67a914b9ae38202a93362265fcaa8f2d621e
Tomi Kartaslammi <tkartasl@c1r1p6.hive.fi>
2023-11-28 16:12:58 +0200

    update

--- GetNextLine/get_next_line.c
+++ GetNextLine/get_next_line.c
@@ -6,118 +6,104 @@
 /*   By: tkartasl <tkartasl@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 09:43:07 by tkartasl          #+#    #+#             */
-/*   Updated: 2023/11/23 14:32:24 by tkartasl         ###   ########.fr       */
+/*   Updated: 2023/11/28 16:06:56 by tkartasl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
 #include <stdio.h>
 
-/*static void	ft_get_extra(char *buffer)
+static void	ft_get_remain(char *buffer)
 {
-	int 	i;
+	int		i;
 	int		j;
-	char	tempbuffer[BUFFER_SIZE + 1];
+	char	temp[BUFFER_SIZE];
 
-	//printf("buffer before get extra: %s\n", buffer);
 	j = 0;
 	i = 0;
-	while (buffer[j] != '\n' && buffer[j] != 0)
-		j++;
-	j++;
-	if (ft_strchr(buffer, '\n') == 0)
-		j = 0;
-	while (buffer[j] != 0)
-	{
-		tempbuffer[i] = buffer[j];
+	while (buffer[i] != '\n' && buffer[i] != 0)
 		i++;
-		j++;
-	}
-	i = 0;
-	while (i < j)
+	if (buffer[i] == '\n')
 	{
-		buffer[i] = tempbuffer[i];
 		i++;
-	}
-	buffer[i] = 0;
-	//printf("buffer after get extra: %s\n", buffer);
-}*/
-
-static char	*ft_clean_end(char *str, char *buffer)
-{
-	unsigned int	i;
-	size_t			len;
-	int				j;
-	char			*temp;
-
-	temp = str;
-	j = 0;
-	len = 0;
-	i = 0;
-	while (temp[len] != '\n' && temp[len] != 0)
-		len++;
-	ft_bzero(buffer, BUFFER_SIZE + 1);
-	if (temp[len] == 0)
-		return (temp);
-    ft_bzero(buffer, BUFFER_SIZE + 1);
-	j = len + 1;
-    while (temp[j] != 0)
-		buffer[i++] = temp[j++];
-	//free (temp);
-	temp = ft_substr(str, 0, len + 1);
-	if (*temp == 0)
-	{
-		free(temp);
-		return (0);
-	}
-	free (str);
-	return (temp);
+		while (buffer[i] != 0)
+			temp[j++] = buffer[i++];
+		i = 0;
+		while (i < j)
+		{
+			buffer[i] = temp[i];
+			i++;
+		}
+		buffer[i] = 0;
+	} 
+	else
+		ft_bzero(buffer, BUFFER_SIZE + 1);
 }
 
-static char	*ft_cpy_line(int fd, char *buffer)
+static char	*ft_trim_line(char *str)
 {
-	char	*str;
-	size_t	bcount;
+	int		len;
+	char	*temp;
 
-	bcount = 1;
-	str = ft_substr(buffer, 0, ft_strlen(buffer));
+	temp = 0;
+	len = 0;
+	while (str[len] != '\n' && str[len] != 0)
+		len++;
+	if (str[len] == '\n')
+		len++;
+	temp = ft_strndup(str, len);
+	free (str);
+	str = temp;
+/*	else
+	{
+		temp = ft_strndup(str, len);
+		free (str);
+		str = temp;
+	}*/
+	return (str);
+}
+
+static char	*ft_cpy_line(int fd, char *buffer, char *str, size_t bcount)
+{
+	char	*line;
+
+	line = 0;
+	if (buffer[0] != 0)
+		str = ft_strndup(buffer, ft_strlen(buffer));
+	else
+		str = "\0";
 	if (str == 0)
 		return (0);
-	while (ft_strchr(buffer, '\n') == 0 && bcount != 0)
+	while (ft_strchr(buffer, '\n') == 0 && bcount == BUFFER_SIZE)
 	{
-		//ft_bzero(buffer, BUFFER_SIZE + 1);
+		ft_bzero(buffer, BUFFER_SIZE + 1);
 		bcount = read(fd, buffer, BUFFER_SIZE); 
 		if (bcount < 0)
 			return (0);
-		//if (bcount != 0 && ft_strchr(buffer, '\n') == 0)
-		buffer[bcount] = 0;
-		str = ft_strjoin(str, buffer);
+		if (bcount == 0)
+			return (NULL);
+		str = ft_strjoin(str, buffer); 
 		if (str == 0)
-		{
-			free(str);
 			return (0);
-		}
 	}
-	//printf("%s", buffer);
-	return (str);
+	line = ft_trim_line(str);
+	ft_get_remain(buffer);
+	return (line);
 }
 
 char	*get_next_line(int fd)
 {
 	char			*str;
 	static char		buffer[BUFFER_SIZE + 1];
+	size_t			bcount;
 
+	bcount = BUFFER_SIZE;
 	str = 0;
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
-	str = ft_cpy_line(fd, buffer);
-	if (*str == 0)
-    {
-		free(str);
+	str = ft_cpy_line(fd, buffer, str, bcount);
+	if (str == 0)
 		return (0);
-    }
-//	printf("str after copy line: %s\n", str);
-	str = ft_clean_end(str, buffer);
-	//printf("str after clean end: %s\n", str);
-	//ft_get_extra(buffer);
+	if (*str == 0)
+		return (0);
 	return (str);
 }
--- GetNextLine/get_next_line.h
+++ GetNextLine/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: tkartasl <tkartasl@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 14:22:55 by tkartasl          #+#    #+#             */
-/*   Updated: 2023/11/23 12:45:13 by tkartasl         ###   ########.fr       */
+/*   Updated: 2023/11/28 15:06:37 by tkartasl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef GET_NEXT_LINE_H
@@ -19,11 +19,11 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+char	*ft_strndup(const char *s1, int len);
 void	ft_bzero(void *s, size_t n);
 size_t	ft_strlen(const char *s);
 char	*ft_strchr(const char *s, int c);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strjoin(char *s1, char *s2);
 char	*get_next_line(int fd);
 
 #endif
